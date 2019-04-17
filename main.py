8e9266731b3ed5790b1dceedc4b7e2b6b458fda6
Manali Jain <jainmanali05@gmail.com>
2019-04-16 17:33:43 -0700

    sleep changes

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
         # bin.updateBin(10)
         GPIO.output(TRIG, False)                 #Set TRIG as LOW
         print("Waitng For Sensor To Settle")
-        time.sleep(2)                            #Delay of 2 seconds
+        # time.sleep(2)                            #Delay of 2 seconds
 
         GPIO.output(TRIG, True)                  #Set TRIG as HIGH
         time.sleep(0.00001)                      #Delay of 0.00001 seconds
@@ -36,9 +36,11 @@ if __name__ == "__main__":
         distance = pulse_duration * 17150        #Multiply pulse duration by 17150 to get distance
         distance = round(distance, 2)            #Round to two decimal points
 
-        if distance > 2 and distance < 300:      #Check whether the distance is within range
-            print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
-            bin.updateBin(distance)
-            # bin.updateBin(10)
-        else:
-            print "Out Of Range"                 #display out of range
+        bin.updateBin(distance)
+
+        # if distance > 2 and distance < 300:      #Check whether the distance is within range
+        #     print "Distance:",distance - 0.5,"cm"  #Print distance with 0.5 cm calibration
+        #     bin.updateBin(distance)
+        #     # bin.updateBin(10)
+        # else:
+        #     print "Out Of Range"                 #display out of range
