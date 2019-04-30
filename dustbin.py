5e088d30646b973dcf1e8c072ffe973068379976
ariman01 <arijit.mandal@sjsu.edu>
2019-04-30 16:51:28 -0700

    enabling message service

--- dustbin.py
+++ dustbin.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient;
 from bson.objectid import ObjectId
-#from twilio.rest import Client
+from twilio.rest import Client
 import datetime
 
 class DustBin:
@@ -18,13 +18,13 @@ class DustBin:
         self.collection = self.db[self.mongo_db_collection]
         self.trash_log_collection = self.db[self.mongo_db_collection_log]
 
-    '''def sendSMS(self):
+    def sendSMS(self):
         account_sid = 'AC67863e4c5ec2c424ecf5cc26ec466d36'
         auth_token = '7a7f1773699682ed4de7b8b1b976ac59'
         client = Client(account_sid, auth_token)
         msg = "The bin placed at " + " MLK LIbrary" + " is about to get completely filled. It needs assitance."
         message = client.messages.create(body = msg,from_ = '+15128569912',to = '+15122010228')
-        print("Message SID :",message.sid)'''
+        print("Message SID :",message.sid)
 
 
     def updateBin(self,height):
@@ -42,8 +42,8 @@ class DustBin:
                 self.trash_log_collection.insert_one({"max_height":self.bin_max_height,"current_height":(self.bin_max_height - height), "timestamp": now,"bin_id":self.bin_id})
                 self.prev_dustbin_height = height
                 if( height <= 5):
-                    print("Dustbin is almost full !!!")
-                    #self.sendSMS()
+                    print("Dustbin is almost full, sending sms alert !!!")
+                    self.sendSMS()
             else:
                 print("Nothing got added !!!")
             print("\n_______________________________________________________________\n")
