5a8a03445cc867447dacd96b550cc380d3002bfc
ariman01 <arijit.mandal@sjsu.edu>
2019-05-01 17:11:22 -0700

    updating dustbin height.

--- dustbin.py
+++ dustbin.py
@@ -23,7 +23,7 @@ class DustBin:
         auth_token = 'c8a6dc2aa268c86b009a5923f0dae718'
         client = Client(account_sid, auth_token)
         msg = "The bin placed at " + " engineering bld. " + " is about to get completely filled. It needs assitance."
-        message = client.messages.create(body = msg,from_ = '+12024101410',to = '+6692526739')
+        message = client.messages.create(body = msg,from_ = '+12024101410',to = '+16692526739')
         print("Message SID :",message.sid)
 
 
